64e83484a2faa7f813f78d8699582c863063c5ab
Maheshwaran Umapathy <wys925@my.utsa.edu>
2019-08-22 12:51:35 -0500

    test added

---
+++ tests/test_sum.py
@@ -0,0 +1,13 @@
+import unittest
+from functions import sum_
+
+
+class TestSum(unittest.TestCase):
+	def test_list_int(self):
+		data = [1, 2, 3]
+		result = sum(data)
+		self.assertEquals(result, 6)
+
+
+if __name__ == "__main__":
+	unittest.main()
